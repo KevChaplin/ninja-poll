d57f2223cfb10400345d049f8a46eff53a08fd60
KevChaplin <kevchaplin44@gmail.com>
2021-06-15 09:22:48 +0900

    initial commit
    develpoment complete, awaiting production build

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ninja PollList
+## Not own work - follow-along YouTube tutorial from the Net Ninja
+### https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg
+
+Created using Svelte  
+Tutorial series to introduce the basics of Svelte  
+Stored here for personal reference
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import PollList from './components/PollList.svelte'
+	import Tabs from './shared/Tabs.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
+
+	//tabs
+	let items = ['Current Polls', 'Add New Polls']
+	let activeItem = 'Current Polls'
+	const tabChange = (e) => {
+		activeItem = e. detail;
+	}
+
+	const handleAdd = (e) => {
+		activeItem = 'Current Polls';
+	}
+</script>
+
+<Header />
+<main>
+	<Tabs  {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls'}
+		<PollList />
+	{:else if activeItem === 'Add New Polls'}
+		<CreatePollForm on:add={handleAdd} />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
